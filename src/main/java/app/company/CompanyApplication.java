685e65a1eabfac15636479f8dc89bf9b575ebecd
Hassan <hassanb.2jz@gmail.com>
2023-08-20 23:23:53 +0300

    [BE commit 9 - Hassan][updated configuration for CORS policy]

--- src/main/java/app/company/CompanyApplication.java
+++ src/main/java/app/company/CompanyApplication.java
@@ -3,11 +3,21 @@ package app.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CompanyApplication {
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyApplication.class, args);
 	}
--- src/main/java/app/company/employee/controller/EmployeeController.java
+++ src/main/java/app/company/employee/controller/EmployeeController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Validated
 @RequestMapping("/api/employee")
-@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
     private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
     private final EmployeeService employeeService;
